9e3c83247a14e36ac4cb1329ded7abcd2622fd5f
H.Hokari <hookhull@gmail.com>
2022-01-22 02:05:59 +0900

    ordered js one more

--- dist/main.js
+++ dist/main.js
@@ -1,2 +1,105 @@
-(()=>{"use strict";const e=document.querySelector(".e_hamurerMenu_button"),s=document.querySelector(".c_hamburgerMenu");e.addEventListener("click",(()=>{let e=document.querySelector(".e_icon-t"),o=document.querySelector(".e_icon-m"),t=document.querySelector(".e_icon-b");s.classList.contains("open")?(s.classList.remove("open"),e.classList.remove("open"),t.classList.remove("open"),o.classList.remove("open")):(s.classList.add("open"),e.classList.add("open"),t.classList.add("open"),o.classList.add("open"))}))})();
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/style.scss":
+/*!************************!*\
+  !*** ./src/style.scss ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ "./src/style.scss");
+
+
+const body = document.querySelector('.l_body')
+const button = document.querySelector('.e_hamurerMenu_button')
+const hamburgerMenu = document.querySelector('.c_hamburgerMenu')
+const iconT = document.querySelector('.e_icon-t');
+const iconM = document.querySelector('.e_icon-m');
+const iconB = document.querySelector('.e_icon-b');
+
+const handleOpen = () => {
+  hamburgerMenu.classList.add('open');
+  iconT.classList.add('open');
+  iconB.classList.add('open');
+  iconM.classList.add('open');
+}
+
+const handleClose = () => {
+  hamburgerMenu.classList.remove('open')
+  iconT.classList.remove('open');
+  iconB.classList.remove('open');
+  iconM.classList.remove('open');
+}
+
+const toggleMenu = () => {
+  if (hamburgerMenu.classList.contains('open')) {
+    handleClose()
+  } else {
+    handleOpen()
+  }
+}
+
+const hamburger = () => {
+  button.addEventListener('click', toggleMenu)
+  // body.addEventListener('click', handleClose)
+}
+
+hamburger()
+})();
+
+/******/ })()
+;
 //# sourceMappingURL=main.js.map
--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 import './style.scss'
 
+const body = document.querySelector('.l_body')
 const button = document.querySelector('.e_hamurerMenu_button')
 const hamburgerMenu = document.querySelector('.c_hamburgerMenu')
+const iconT = document.querySelector('.e_icon-t');
+const iconM = document.querySelector('.e_icon-m');
+const iconB = document.querySelector('.e_icon-b');
 
-const handleToggle = () => {
-  let iconT = document.querySelector('.e_icon-t');
-  let iconM = document.querySelector('.e_icon-m');
-  let iconB = document.querySelector('.e_icon-b');
+const handleOpen = () => {
+  hamburgerMenu.classList.add('open');
+  iconT.classList.add('open');
+  iconB.classList.add('open');
+  iconM.classList.add('open');
+}
 
+const handleClose = () => {
+  hamburgerMenu.classList.remove('open')
+  iconT.classList.remove('open');
+  iconB.classList.remove('open');
+  iconM.classList.remove('open');
+}
+
+const toggleMenu = () => {
   if (hamburgerMenu.classList.contains('open')) {
-    hamburgerMenu.classList.remove('open');
-    iconT.classList.remove('open');
-    iconB.classList.remove('open');
-    iconM.classList.remove('open');
+    handleClose()
   } else {
-    hamburgerMenu.classList.add('open');
-    iconT.classList.add('open');
-    iconB.classList.add('open');
-    iconM.classList.add('open');
+    handleOpen()
   }
 }
 
-button.addEventListener('click', handleToggle)
+const hamburger = () => {
+  button.addEventListener('click', toggleMenu)
+  // body.addEventListener('click', handleClose)
+}
+
+hamburger()
