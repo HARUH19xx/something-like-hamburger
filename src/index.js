6d0fb0846fa2f03bca5add0de2618ba322a87bd2
H.Hokari <hookhull@gmail.com>
2022-01-21 23:53:34 +0900

    ordered js

--- src/index.js
+++ src/index.js
@@ -1,35 +1,24 @@
 import './style.scss'
 
-document.querySelector('.e_hamurerMenu_button').addEventListener('click', () => {
+const button = document.querySelector('.e_hamurerMenu_button')
+const hamburgerMenu = document.querySelector('.c_hamburgerMenu')
 
-    let iconT = document.querySelector('.e_icon-t');
-    let iconM = document.querySelector('.e_icon-m');
-    let iconB = document.querySelector('.e_icon-b');
-  
-    if (iconT.classList.contains('open') == true) {
-      iconT.classList.remove('open');
-    } else {
-      iconT.classList.add('open');
-    }
+const handleOpen = () => {
+  let iconT = document.querySelector('.e_icon-t');
+  let iconM = document.querySelector('.e_icon-m');
+  let iconB = document.querySelector('.e_icon-b');
 
-    if (iconB.classList.contains('open') == true) {
-      iconB.classList.remove('open');
-    } else {
-      iconB.classList.add('open');
-    }
-  
-    if (iconM.classList.contains('open') == true) {
-      iconM.classList.remove('open');
-    } else {
-      iconM.classList.add('open');
-    }
-  
-    let hamburgerMenu = document.querySelector('.c_hamburgerMenu');
-  
-    if (hamburgerMenu.classList.contains('open') == true) {
-      hamburgerMenu.classList.remove('open');
-    } else {
-      hamburgerMenu.classList.add('open');
-    }
-  
-  })
+  if (hamburgerMenu.classList.contains('open')) {
+    hamburgerMenu.classList.remove('open');
+    iconT.classList.remove('open');
+    iconB.classList.remove('open');
+    iconM.classList.remove('open');
+  } else {
+    hamburgerMenu.classList.add('open');
+    iconT.classList.add('open');
+    iconB.classList.add('open');
+    iconM.classList.add('open');
+  }
+}
+
+button.addEventListener('click', handleOpen)
