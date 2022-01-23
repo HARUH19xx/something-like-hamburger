c6d56ac91719e9f164c8ee8e0a3e0950b0bab500
H.Hokari <hookhull@gmail.com>
2022-01-23 12:13:50 +0900

    make menu-box close when to click outside of the box or the anchors in the box

--- dist/main.js
+++ dist/main.js
@@ -63,13 +63,16 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var _style_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.scss */ "./src/style.scss");
 
 
-const body = document.querySelector('.l_body')
+//定義
 const button = document.querySelector('.e_hamurerMenu_button')
 const hamburgerMenu = document.querySelector('.c_hamburgerMenu')
+const hamburgerOption = document.querySelectorAll('.e_hamburgerMenu_box a')
 const iconT = document.querySelector('.e_icon-t');
 const iconM = document.querySelector('.e_icon-m');
 const iconB = document.querySelector('.e_icon-b');
+const outerHamburger = document.querySelector('.outerHamburger')
 
+//展開
 const handleOpen = () => {
   hamburgerMenu.classList.add('open');
   iconT.classList.add('open');
@@ -94,9 +97,13 @@ const toggleMenu = () => {
 
 const hamburger = () => {
   button.addEventListener('click', toggleMenu)
-  // body.addEventListener('click', handleClose)
+  for (let i = 0 ; i < hamburgerOption.length ; i++) {
+    hamburgerOption[i].addEventListener('click', handleClose)
+  }
+  outerHamburger.addEventListener('click', handleClose)
 }
 
+//帰結
 hamburger()
 })();
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './style.scss'
 
-const body = document.querySelector('.l_body')
+//定義
 const button = document.querySelector('.e_hamurerMenu_button')
 const hamburgerMenu = document.querySelector('.c_hamburgerMenu')
+const hamburgerOption = document.querySelectorAll('.e_hamburgerMenu_box a')
 const iconT = document.querySelector('.e_icon-t');
 const iconM = document.querySelector('.e_icon-m');
 const iconB = document.querySelector('.e_icon-b');
+const outerHamburger = document.querySelector('.outerHamburger')
 
+//展開
 const handleOpen = () => {
   hamburgerMenu.classList.add('open');
   iconT.classList.add('open');
@@ -31,7 +34,11 @@ const toggleMenu = () => {
 
 const hamburger = () => {
   button.addEventListener('click', toggleMenu)
-  // body.addEventListener('click', handleClose)
+  for (let i = 0 ; i < hamburgerOption.length ; i++) {
+    hamburgerOption[i].addEventListener('click', handleClose)
+  }
+  outerHamburger.addEventListener('click', handleClose)
 }
 
+//帰結
 hamburger()
